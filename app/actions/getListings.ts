d8b75cc34c7a457af2020af8ad5103641015e5d6
StarkLSH <seungheon1029@gmail.com>
2023-06-12 00:46:34 +0900

    feat: listing 로직 수정

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -15,12 +15,12 @@ export default async function getListings(params: IListingsParams) {
   try {
     const {
       userId,
-      guestCount,
       roomCount,
+      guestCount,
       bathroomCount,
+      locationValue,
       startDate,
       endDate,
-      locationValue,
       category,
     } = params;
 
@@ -75,18 +75,12 @@ export default async function getListings(params: IListingsParams) {
       };
     }
 
-    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-    console.log(query);
-
     const listings = await prisma.listing.findMany({
       where: query,
       orderBy: {
         createdAt: "desc",
       },
     });
-
-    console.log("================================");
-    console.log(listings);
 
     const safeListings = listings.map((listing) => ({
       ...listing,
